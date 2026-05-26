ebe16ed85e7298a27364ae86bb9c3d462f5ae24f
hbg1901 <bgharshith42@gmail.com>
2026-05-26 11:39:07 +0530

    Update User.sql

--- Analysis/User.sql
+++ Analysis/User.sql
@@ -1,1 +1,18 @@
-1
+-- ============================================
+-- SCENARIO: User lookup production
+-- ============================================
+
+SELECT username,*
+FROM prod_user_identity_master_x22
+WHERE user_account_id = :user_id;
+
+
+
+-- ============================================
+-- SCENARIO: Recently created users
+
+-- ============================================
+
+SELECT *
+FROM prod_user_identity_master_x22
+WHERE created_time >= NOW() - INTERVAL '48 HOURS';
